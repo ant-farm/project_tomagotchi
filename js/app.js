5220381ed23d817e38ae495d3743e04339021cc3
Anthony Torres <anthony_torres@me.com>
2019-10-01 15:40:54 -0500

    instantiated my Tomagotchi

---
+++ js/app.js
@@ -0,0 +1,36 @@
+
+class Tomagotchi {
+		constructor(name, hair){
+			this.legs = 4;
+			this.name = name;
+			this.color= hair;
+		}
+	}
+// 		die(){
+
+// 		}
+// 		sleep(){
+
+// 		}
+// 		eat(){
+
+// 		}
+
+const jim = new Tomagotchi('Jim','green');
+
+console.log(jim);
+const game = {
+
+
+
+// time function 
+
+// create a function to print hugar level... make sure you write these functions generally
+
+//do not have more than one interval  
+
+};
+
+// listeners... only use methods down here outside of the game function, use these clicks etc on the methods above
+
+// create a button 
