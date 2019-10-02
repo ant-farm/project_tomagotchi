12fd44752b7ffe4a0d3e80e48d55060a279cde7b
Anthony Torres <anthony_torres@me.com>
2019-10-02 11:24:21 -0500

    created a form submission that takes the user input and adds the name to the new tamagotchi

--- js/app.js
+++ js/app.js
@@ -1,85 +1,89 @@
 
 class Tamagotchi {
-		constructor(name, hair){
+		constructor(name){
 			this.legs = 4;
 			this.hunger = Math.floor(Math.random() * 10) +1;
 			this.sleepiness = Math.floor(Math.random() * 10) +1;
 			this.boredom = Math.floor(Math.random() * 10) +1;
 			this.name = name;
-			this.color= hair;
 			this.age = 0;
+			this.time = 10;
 			
 		}
 	}
 
+// time(){
+// 	const $timer = $('.timer')
 
-	
+// 	const interval = setInterval (() => {
+		
+// 		if(this.timer === 10 ){
+// 			clearInterval(interval);
+// 		} else {
+// 			this.timer++
+// 		}
+// 	},1000)
+// },
 
-// $('.stats').append('Stats:' + game.Tamagotchi.hunger)
 
 const game = {
-// const newTom = ''
-createTamagotchi(){
-	const newTom = new Tamagotchi;
-	const $p = $('<p/>');
-	const $div = $('<div/>') 
-	// const $health = $('.health-stats')
-	const $hunger = $('.hunger');
-	const $sleepiness = $('.sleepiness')
-	const $boredom = $('.boredom')
-	// $p.append('Health:');
-	$('.hunger').text('Hunger: ')
-	$('.hunger').append(newTom.hunger)
-	$('.sleepiness').text('Sleepiness level: ')
-	$('.sleepiness').append(newTom.sleepiness)
-	$('.boredom').text('Boredom level: ')
-	$('.boredom').append(newTom.boredom)
 
-	// $('.timer').append(this.timer())
+	createTamagotchi(name){
+		const newTom = new Tamagotchi(name);
+		console.log(newTom);
+		$('.hunger').text('Hunger: ')
+		$('.hunger').append(newTom.hunger)
+		$('.sleepiness').text('Sleepiness level: ')
+		$('.sleepiness').append(newTom.sleepiness)
+		$('.boredom').text('Boredom level: ')
+		$('.boredom').append(newTom.boredom)
+		$('.timer').text('Timer: ')
+		$('.timer').append(newTom.time)
 
-	// game.timer();
-},
-// prompt the player by welcoming them to the game and asking they input a name for the Tomagotchi.
-// userPrompt(){
+		// $('.timer').append(this.timer())
 
-// 	let userInput = prompt("Welcome to your Tamagotchi! What would you like to name you're pet?")
-// 	return new Tamagotchi(userInput, 'green')
+		// game.timer();
+	},
+	// prompt the player by welcoming them to the game and asking they input a name for the Tomagotchi.
+	// userPrompt(){
 
-// },
+	// 	let userInput = prompt("Welcome to your Tamagotchi! What would you like to name you're pet?")
+	// 	return new Tamagotchi(userInput, 'green')
 
-// create a function that will start incrementing the time up to 5 minutes. This will be part of the feeding, sleeping and playing functions as well. Every 15 seconds the tomagotchi's levels will go up, if they get to 10 then it dies.
+	// },
 
-
-// time(){
-// 	setInterval ((e.target) => {
-// 		e.currenttarget
-// 	},15000);
-// },
+	// create a function that will start incrementing the time up to 5 minutes. This will be part of the feeding, sleeping and playing functions as well. Every 15 seconds the tomagotchi's levels will go up, if they get to 10 then it dies.
 
 
 
-// create a feeding function. This will be used with an on.click() method down below, outside of the game. Everytime it's clicked the Tomagotchi's hungar level will go up by 1
-feedingTime(){
-$('health-stats').append()
-},
 
-lightsOut(){
-	// to help his sleepiness level. When you turn the light out his level will go down 1
-},
 
-play(){
 
-}
+
+	// create a feeding function. This will be used with an on.click() method down below, outside of the game. Everytime it's clicked the Tomagotchi's hungar level will go up by 1
+	feedingTime(){
+		$('health-stats').append()
+	},
+
+	lightsOut(){
+		// to help his sleepiness level. When you turn the light out his level will go down 1
+	},
+
+	play(){
+
+	}
 
 
 };
 
 // Event listeners --------------------------
 
-$('.startGame').on('click',() => {
-	console.log('button Works');
-	game.createTamagotchi();
-})
+$('form').on('submit',(event) => {
+	event.preventDefault();
+	const input = $('#input-box').val()
+	game.createTamagotchi(input);
+	// input.hide();
+});
 
 
 
